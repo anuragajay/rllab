1117f9b0179e6c8219fe834b8dc518306988e4ad
anuragajay <anuragajay@berkeley.edu>
2016-09-02 00:17:33 -0700

    peg insertion task added

---
+++ examples/trpo_mjc.py
@@ -0,0 +1,27 @@
+from rllab.algos.trpo import TRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.mujoco.peg_env import PegEnv
+from rllab.envs.normalized_env import normalize
+from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
+
+env = normalize(PegEnv())
+
+policy = GaussianMLPPolicy(
+    env_spec=env.spec,
+    # The neural network policy should have two hidden layers, each with 32 hidden units.
+    hidden_sizes=(32, 32)
+)
+
+baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+algo = TRPO(
+    env=env,
+    policy=policy,
+    baseline=baseline,
+    batch_size=20000,
+    max_path_length=100,
+    n_itr=1000,
+    discount=0.99,
+    step_size=0.01,
+)
+algo.train()
