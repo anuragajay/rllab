5b0cdc024902a589d4f8ec2b1dbc5dc507246233
anuragajay <anuragajay@berkeley.edu>
2016-09-02 18:09:19 -0700

    trpo working with peg insertion

--- examples/trpo_mjc.py
+++ examples/trpo_mjc.py
@@ -3,6 +3,9 @@ from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from rllab.envs.mujoco.peg_env import PegEnv
 from rllab.envs.normalized_env import normalize
 from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
+from rllab.misc.instrument import stub, run_experiment_lite
+
+stub(globals())
 
 env = normalize(PegEnv())
 
@@ -18,10 +21,24 @@ algo = TRPO(
     env=env,
     policy=policy,
     baseline=baseline,
-    batch_size=45000,
+    batch_size=90000,
     max_path_length=100,
     n_itr=1000,
     discount=0.99,
     step_size=0.01,
+    plot=True,
+    optimizer_args={
+        'cg_iters': 100
+    }
 )
-algo.train()
+
+run_experiment_lite(
+    algo.train(),
+    # Number of parallel workers for sampling
+    n_parallel=8,
+    # Only keep the snapshot parameters for the last iteration
+    snapshot_mode="last",
+    # Specifies the seed for the experiment. If this is not provided, a random seed
+    # will be used,
+    plot=True,
+)
