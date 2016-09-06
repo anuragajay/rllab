d7fd6a43e49f50f07bc2b11829d71c6699a4beea
anuragajay <anuragajay@berkeley.edu>
2016-09-05 19:19:31 -0700

    some updates

--- examples/rwr_mjc.py
+++ examples/rwr_mjc.py
@@ -1,6 +1,6 @@
-from rllab.algos.trpo import TRPO
-from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.algos.erwr import ERWR
 from rllab.envs.mujoco.peg_env import PegEnv
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
 from rllab.envs.normalized_env import normalize
 from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
 from rllab.misc.instrument import stub, run_experiment_lite
@@ -21,7 +21,7 @@ baseline = LinearFeatureBaseline(env_spec=env.spec)
 
 vg = instrument.VariantGenerator()
 vg.add("seed", [1,2,3])
-vg.add("step_size",[0.1])#,0.01,0.001])
+vg.add("step_size",[0.1])
 
 variants = vg.variants()
 num = eval(sys.argv[1])
@@ -30,22 +30,18 @@ print "#Experiments number:", num
 variant = variants[num]
 
 
-algo = TRPO(
+algo = ERWR(
     env=env,
     policy=policy,
     baseline=baseline,
     batch_size=90000,
     max_path_length=100,
+    step_size=variant["step_size"],
     n_itr=1000,
     discount=0.99,
-    step_size=variant["step_size"],
-    optimizer_args={
-        'cg_iters': 100
-    },
     plot=True,
 )
 
-algo = 
 run_experiment_lite(
     algo.train(),
     # Number of parallel workers for sampling
@@ -56,6 +52,6 @@ run_experiment_lite(
     # Specifies the seed for the experiment. If this is not provided, a random seed
     # will be used,
     seed=variant["seed"],
-    exp_prefix="trpo_peg_search",
-    exp_name="seed_{0}_step_size_{1}".format(variant["seed"], variant["step_size"]),
+    exp_prefix="rwr_peg_search",
+    exp_name="seed_{0}".format(variant["seed"]),
 )
